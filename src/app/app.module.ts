f53ace8a0cd82d3fc584989a5588132f1592a90d
MoneyFactoryCG <moneyfactory.creativgroup@gmail.com>
2019-06-25 18:14:44 +0300

    - test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WorksPageComponent } from './works-page/works-page.component';
+import { CarouselComponent } from './works-page/carousel/carousel.component';
+
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { MastersPageComponent } from './masters-page/masters-page.component';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WorksPageComponent,
+    CarouselComponent,
+    MastersPageComponent
   ],
   imports: [
+    SwiperModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/works-page/carousel/carousel.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+
+import {
+  SwiperComponent,
+  SwiperDirective,
+  SwiperConfigInterface,
+  SwiperScrollbarInterface,
+  SwiperPaginationInterface
+} from "ngx-swiper-wrapper";
+
+@Component({
+  selector: "app-carousel",
+  templateUrl: "./carousel.component.html",
+  styleUrls: ["./carousel.component.scss"]
+})
+export class CarouselComponent implements OnInit {
+  public slides = [
+    "First slide",
+    "Second slide",
+    "Third slide",
+    "Fourth slide",
+    "Fifth slide",
+    "Sixth slide"
+  ];
+
+  public index = 1;
+
+  public config: SwiperConfigInterface = {
+    //a11y: true,
+    // effect: 'coverflow',
+    spaceBetween: 20,
+    navigation: false,
+    slidesPerView: 3,
+    centeredSlides: true,
+    // coverflowEffect: {
+    //   rotate: 0,
+    //   stretch: 0,
+    //   depth: 200,
+    //   modifier: 1,
+    //   slideShadows: true
+    // },
+    loop: true
+  };
+
+  @ViewChild(SwiperComponent, { static: false }) componentRef?: SwiperComponent;
+  @ViewChild(SwiperDirective, { static: false }) directiveRef?: SwiperDirective;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  handleNextSlide() {
+    this.directiveRef.nextSlide();
+  }
+
+  handlePrevSlide() {
+    this.directiveRef.prevSlide();
+  }
+}
