779122443a284c1b924160a14e28cd84dbe995f5
MoneyFactoryCG <moneyfactory.creativgroup@gmail.com>
2019-07-03 18:06:33 +0300

    - add accordion

--- src/app/reviews-page/reviews-carousel/reviews-carousel.component.ts
+++ src/app/reviews-page/reviews-carousel/reviews-carousel.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import {
+  SwiperComponent,
+  SwiperDirective,
+  SwiperConfigInterface,
+  SwiperScrollbarInterface,
+  SwiperPaginationInterface
+} from "ngx-swiper-wrapper";
 
 @Component({
-  selector: 'app-reviews-carousel',
-  templateUrl: './reviews-carousel.component.html',
-  styleUrls: ['./reviews-carousel.component.scss']
+  selector: "app-reviews-carousel",
+  templateUrl: "./reviews-carousel.component.html",
+  styleUrls: ["./reviews-carousel.component.scss"]
 })
 export class ReviewsCarouselComponent implements OnInit {
+  config: SwiperConfigInterface = {
+    pagination: {
+      el: ".swiper-pagination",
+      type: 'custom',
+      renderCustom: (swiper, current, total) => {
+        return this.customProgressBar(current, total);
+      }
+    },
+    a11y: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    spaceBetween: 30,
+    slidesPerView: 2
+    //centeredSlides: true
+  };
 
-  constructor() { }
+  slides = [];
 
-  ngOnInit() {
+  customProgressBar(current: number, total: number): string {
+    const ratio: number = current / total;
+
+    const progressBarStyle: string =
+      "style='transform: translate3d(0px, 0px, 0px) scaleX(" +
+      ratio +
+      ") scaleY(1); transition-duration: 300ms; background: #942FE5;'";
+    const progressBar: string =
+      "<span class='swiper-pagination-progressbar-fill' " +
+      progressBarStyle +
+      "></span>";
+
+    let progressBarContainer: string =
+      "<div class='swiper-pagination-progressbar' style='height: 4px; top: 6px; width: 100%; background-color: #fff;'>";
+    progressBarContainer += progressBar;
+    progressBarContainer += "</span></div>";
+
+    return progressBarContainer;
   }
 
+  constructor() {}
+
+  ngOnInit() {
+    for (let i = 1; i <= 5; i++) {
+      for (let i = 1; i <= 2; i++) {
+        this.slides.push({ img: `../../../assets/imgs/reviews-page/${i}.png` });
+      }
+    }
+  }
 }
--- src/app/reviews-page/reviews-page.component.ts
+++ src/app/reviews-page/reviews-page.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-reviews-page',
-  templateUrl: './reviews-page.component.html',
-  styleUrls: ['./reviews-page.component.scss']
+  selector: "app-reviews-page",
+  templateUrl: "./reviews-page.component.html",
+  styleUrls: ["./reviews-page.component.scss"]
 })
 export class ReviewsPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  isToggle: boolean = false;
+
+  handleClick(event: Event) {
+
+    
+    if (this.isToggle === false) {
+      
+      $("." + event["path"][1].classList[1] + " .content").slideToggle(1000).css("display","flex");
+      this.isToggle = true;
+      $("." + event["path"][1].classList[1] + " .arrow").addClass("active");
+    } else {
+      $("." + event["path"][1].classList[1] + " .content").slideUp(1000);
+      this.isToggle = false;
+      $("." + event["path"][1].classList[1] + " .arrow").removeClass("active");
+    }
+    //console.log("Click", event["path"]);
   }
 
+  ngOnInit() {}
 }
