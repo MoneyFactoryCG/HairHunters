a68a9a5840e50e0b8373180b0a03f19590febaf2
Work 4 <Work4@Work4-iMac.local>
2019-08-13 09:31:29 +0300

    test

--- api/index.js
+++ api/index.js
@@ -4,14 +4,14 @@ const https = require('https');
 const app = require("./app");
 const port = process.env.PORT || 5000;
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/fullchain.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/privkey.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/chain.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
 	cert: certificate,
-	// ca: ca
+	ca: ca
 };
 
 const httpServer = http.createServer(app);
