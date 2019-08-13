233451a23bd532ce3c9d362950e135be56a86cd9
Work 4 <Work4@Work4-iMac.local>
2019-08-13 09:24:04 +0300

    test

--- api/index.js
+++ api/index.js
@@ -17,11 +17,11 @@ const credentials = {
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(5000, () => {
+httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-httpsServer.listen(5000, () => {
+httpsServer.listen(443, () => {
 	console.log('HTTPS Server running on port 443');
 });
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://narashivanie-volos.club:5000'
+  api: 'https://narashivanie-volos.club'
 };
