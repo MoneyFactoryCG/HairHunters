d1b9392aad6642d69eeda8e8024fee22ce2066b7
Work 4 <Work4@Work4-iMac.local>
2019-08-13 09:21:29 +0300

    - test api server

--- api/index.js
+++ api/index.js
@@ -1,3 +1,28 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const app = require("./app");
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server has been started on ${port}`));
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/fullchain.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/privkey.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/narashivanie-volos.club/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	// ca: ca
+};
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(5000, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(5000, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+// app.listen(port, () => console.log(`Server has been started on ${port}`));
