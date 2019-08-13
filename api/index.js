1bbf2d8fee321ba1fae6d285911d0d180c3b57a5
Work 4 <Work4@Work4-iMac.local>
2019-08-13 10:06:15 +0300

    test

--- api/index.js
+++ api/index.js
@@ -21,8 +21,8 @@ const port = process.env.PORT || 5000;
 // 	console.log('HTTP Server running on port 80');
 // });
 
-// httpsServer.listen(5000, () => {
-// 	console.log('HTTPS Server running on port 443');
-// });
+httpsServer.listen(5000, () => {
+	console.log('HTTPS Server running on port 5000');
+});
 
-app.listen(port, () => console.log(`Server has been started on ${port}`));
+// app.listen(port, () => console.log(`Server has been started on ${port}`));
