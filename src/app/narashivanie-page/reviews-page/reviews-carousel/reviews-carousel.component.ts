0b60feda90c8a6d9a4a74047759df3bbff1eb584
MoneyFactoryCG <moneyfactory.creativgroup@gmail.com>
2019-10-07 11:50:44 +0300

    imgs seo

--- src/app/narashivanie-page/reviews-page/reviews-carousel/reviews-carousel.component.ts
+++ src/app/narashivanie-page/reviews-page/reviews-carousel/reviews-carousel.component.ts
@@ -1,82 +1,102 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 import {
   SwiperComponent,
   SwiperDirective,
   SwiperConfigInterface,
   SwiperScrollbarInterface,
-  SwiperPaginationInterface
-} from "ngx-swiper-wrapper";
+  SwiperPaginationInterface,
+} from 'ngx-swiper-wrapper';
 
 @Component({
-  selector: "app-reviews-carousel",
-  templateUrl: "./reviews-carousel.component.html",
-  styleUrls: ["./reviews-carousel.component.scss"]
+  selector: 'app-reviews-carousel',
+  templateUrl: './reviews-carousel.component.html',
+  styleUrls: ['./reviews-carousel.component.scss'],
 })
 export class ReviewsCarouselComponent implements OnInit {
   config: SwiperConfigInterface = {
     pagination: {
-      el: ".reviews-swiper-pagination",
+      el: '.reviews-swiper-pagination',
       type: 'custom',
       renderCustom: (swiper, current, total) => {
         return this.customProgressBar(current, total);
-      }
+      },
     },
     a11y: true,
     navigation: {
-      nextEl: ".reviews-swiper-button-next",
-      prevEl: ".reviews-swiper-button-prev"
+      nextEl: '.reviews-swiper-button-next',
+      prevEl: '.reviews-swiper-button-prev',
     },
     spaceBetween: 30,
     slidesPerView: 2,
     breakpoints: {
       690: {
-        slidesPerView: 1
-      }
-    }
+        slidesPerView: 1,
+      },
+    },
   };
 
   slides = [
     {
-      img: 'assets/imgs/reviews-page/1.jpg',
-      alt: 'Отзывы Итальянское наращивание микрокапсулами натуральных славянских волос на кератин'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-01.jpg',
+      alt:
+        'нарщивание волос отзывы Итальянское наращивание микрокапсулами натуральных славянских волос на кератин',
     },
     {
-      img: 'assets/imgs/reviews-page/2.jpg',
-      alt: 'Отзывы Испанское холодное наращивание натуральных славянских волос на биогель'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-02.jpg',
+      alt:
+        'нарщивание волос отзывы Испанское холодное наращивание натуральных славянских волос на биогель',
     },
     {
-      img: 'assets/imgs/reviews-page/3.jpg',
-      alt: 'Отзывы Студия по наращиванию натуральных славянских волос харьков'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-03.jpg',
+      alt:
+        'нарщивание волос отзывы Студия по наращиванию натуральных славянских волос харьков',
     },
     {
-      img: 'assets/imgs/reviews-page/4.jpg',
-      alt: 'Отзывы Итальянское наращивание микрокапсулами натуральных славянских волос на кератин'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-04.jpg',
+      alt:
+        'нарщивание волос отзывы Итальянское наращивание микрокапсулами натуральных славянских волос на кератин',
     },
     {
-      img: 'assets/imgs/reviews-page/5.jpg',
-      alt: 'Отзывы Испанское холодное наращивание натуральных славянских волос на биогель'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-05.jpg',
+      alt:
+        'нарщивание волос отзывы Испанское холодное наращивание натуральных славянских волос на биогель',
     },
     {
-      img: 'assets/imgs/reviews-page/6.jpg',
-      alt: 'Отзывы Студия по наращиванию натуральных славянских волос харьков'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-06.jpg',
+      alt:
+        'нарщивание волос отзывы Студия по наращиванию натуральных славянских волос харьков',
     },
     {
-      img: 'assets/imgs/reviews-page/7.jpg',
-      alt: 'Отзывы Итальянское наращивание микрокапсулами натуральных славянских волос на кератин'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-07.jpg',
+      alt:
+        'нарщивание волос отзывы Итальянское наращивание микрокапсулами натуральных славянских волос на кератин',
     },
     {
-      img: 'assets/imgs/reviews-page/8.jpg',
-      alt: 'Отзывы Испанское холодное наращивание натуральных славянских волос на биогель'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-08.jpg',
+      alt:
+        'нарщивание волос отзывы Испанское холодное наращивание натуральных славянских волос на биогель',
     },
     {
-      img: 'assets/imgs/reviews-page/9.jpg',
-      alt: 'Отзывы Студия по наращиванию натуральных славянских волос харьков'
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-09.jpg',
+      alt:
+        'нарщивание волос отзывы Студия по наращиванию натуральных славянских волос харьков',
     },
     {
-      img: 'assets/imgs/reviews-page/10.jpg',
-      alt: 'Отзывы Итальянское наращивание микрокапсулами натуральных славянских волос на кератин'
-    }
+      img:
+        'assets/imgs/reviews-page/narashchivaniye-volos-otzyvy-10.jpg',
+      alt:
+        'нарщивание волос отзывы Итальянское наращивание микрокапсулами натуральных славянских волос на кератин',
+    },
   ];
 
   customProgressBar(current: number, total: number): string {
@@ -89,19 +109,17 @@ export class ReviewsCarouselComponent implements OnInit {
     const progressBar: string =
       "<span class='swiper-pagination-progressbar-fill' " +
       progressBarStyle +
-      "></span>";
+      '></span>';
 
     let progressBarContainer: string =
       "<div class='swiper-pagination-progressbar' style='height: 4px; top: 6px; width: 100%; background-color: #fff;'>";
     progressBarContainer += progressBar;
-    progressBarContainer += "</span></div>";
+    progressBarContainer += '</span></div>';
 
     return progressBarContainer;
   }
 
   constructor() {}
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 }
