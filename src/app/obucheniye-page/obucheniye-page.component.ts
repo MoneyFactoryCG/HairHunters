198db0799c2294f3573107b9c765a15c85ad386a
MoneyFactoryCG <moneyfactory.creativgroup@gmail.com>
2019-10-15 09:35:39 +0300

    add seo service

--- src/app/obucheniye-page/obucheniye-page.component.ts
+++ src/app/obucheniye-page/obucheniye-page.component.ts
@@ -3,6 +3,7 @@ import { Renderer2, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 import * as $ from 'jquery';
+import { SeoServiceService } from '../seo-service.service';
 
 @Component({
   selector: 'app-obucheniye-page',
@@ -11,10 +12,17 @@ import * as $ from 'jquery';
 })
 export class ObucheniyePageComponent
   implements OnInit, AfterViewInit {
-  constructor(
-    @Inject(DOCUMENT) private _document: Document,
-    private _renderer2: Renderer2,
-  ) {}
+  constructor(private seo: SeoServiceService) {
+    this.seo.setTags({
+      title: 'Курсы наращивания волос | Харьков', // Title
+      titleSuffix: '- Hair Hunters', // Title Suffix
+      description:
+        'Индивидуальный курс по наращиванию волос. Холодное наращивание на биогель, наращивание на кератин.', // Description
+      image: 'assets/og-img.jpg', // Image
+      keywords:
+        'нарашивание волос курсы, курсы, наращивание волос, харьков, биогель, кератин, холодное наращивание, горячее наращивание', // Keywords
+    });
+  }
   ngOnInit() {}
   ngAfterViewInit() {
     setTimeout(() => {
